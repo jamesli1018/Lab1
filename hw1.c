8c22996bc843375a432e7d72a110ec5a0b27441b
Jinteng Li <jli3@bertvm.cs.uic.edu>
2018-01-31 10:21:08 -0600

    hw1

---
+++ hw1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("netID:%s\n", "jli246");
+	return 0;
+}
+
+void all()
+{
+	return;	
+}
+
+int your;
+
+void cs361()
+{
+	return;
+}
+
+char are = 'a';
+int belong;
+int to = 0;
+
+typedef struct node{
+char a[37];
+}NODE;
+
+NODE us;
